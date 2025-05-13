b66c8c6cb33e40a4d76d4bf4526bead31c17fd59
Jiaq7 <wzcjq123@163.com>
2025-05-13 15:44:31 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <sys/unistd.h>
+#include <sys/types.h>
+#include <sys/errno.h>
+#include <netinet/in.h>
+#include <signal.h>
 
+#define BUFFSIZE 2048
+#define DEFAULT_PORT 16555    // 指定端口为16555
+#define MAXLINK 2048
 int main(int, char**){
-    std::cout << "Hello, from main!\n";
+    struct sockaddr_in servaddr;    // 用于存放ip和端口的结构
+    char buff[BUFFSIZE];    // 用于收发数据
+
 }
