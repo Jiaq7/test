f90b5529d59d53234a3d8b25e9ce2874803f8a45
Jiaq7 <wzcjq123@163.com>
2025-05-14 14:53:22 +0800

    update0514

--- cilent/main.cpp
+++ cilent/main.cpp
@@ -8,7 +8,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #define BUFFSIZE 2048
-#define SERVER_IP "192.168.19.12"    // 指定服务端的IP，记得修改为你的服务端所在的ip
+#define SERVER_IP "192.168.13.17"    // 指定服务端的IP，记得修改为你的服务端所在的ip
 #define SERVER_PORT 16555            // 指定服务端的port
 
 int main()
