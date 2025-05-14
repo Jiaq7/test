5df78412850e63e3bf17e799040d7ca93b45ad76
Jiaq7 <wzcjq123@163.com>
2025-05-14 10:15:29 +0800

    11

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /root/Documents/demo1/test
+# Install script for directory: /root/Documents/demo1/server
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/root/Documents/demo1/test/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/root/Documents/demo1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
