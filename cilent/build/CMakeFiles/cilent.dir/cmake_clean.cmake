50695ac799f69c78e738b5377543bc9e030a7cc8
Jiaq7 <wzcjq123@163.com>
2025-05-14 15:02:24 +0800

    server_update

---
+++ cilent/build/CMakeFiles/cilent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cilent.dir/main.cpp.o"
+  "CMakeFiles/cilent.dir/main.cpp.o.d"
+  "cilent"
+  "cilent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cilent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
